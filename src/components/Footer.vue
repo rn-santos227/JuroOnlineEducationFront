8bd8e1cc8eba247ad0c0df87c8e5c6e214c12aa7
rn-santos227 <rn_santos227@yahoo.com>
2020-06-08 09:53:34 +0800

    Initial Commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="footer">
+    <v-content class="content"></v-content>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  methods: {}
+};
+</script>
+
+<style>
+#footer {
+  background: #2c6195;
+}
+
+#content {
+  margin: 50px;
+}
+</style>
