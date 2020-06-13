8ef610a9864cc62836e9972c419fdae5e17bc809
rn-santos227 <rn_santos227@yahoo.com>
2020-06-14 00:02:45 +0800

    added footer and header

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,22 +1,89 @@
 <template>
-  <div class="footer">
-    <v-content class="content"></v-content>
-  </div>
+  <v-footer color="#2c6195" dark>
+    <v-flex cols="1">
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="app_download shrink mr-2 footer-log"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+      </div>
+    </v-flex>
+    <v-flex cols="2">
+      <div v-for="link in links" :key="link" class="my-2">
+        <v-btn color="white" rounded text small> {{ link }} </v-btn>
+      </div>
+    </v-flex>
+    <v-flex cols="3">
+      <v-flex class="ml-3 mt-3">
+        <h2 color="white">Download Juro Online Education</h2>
+      </v-flex>
+      <v-flex>
+        <v-btn class="app_download ma-3" color="black" large>
+          <span style="white-space: normal;">
+            Get it on<br />
+            <h3>App Store</h3>
+          </span>
+        </v-btn>
+        <v-btn class="app_download ma-3" color="black" large>
+          <span style="white-space: normal;">
+            Get it on<br />
+            <h3>Google Play</h3>
+          </span>
+        </v-btn>
+      </v-flex>
+      <v-flex>
+        <v-card-text>
+          <v-btn
+            v-for="icon in icons"
+            :key="icon"
+            class="mr-6 mb-3 white--text"
+            icon
+          >
+            <v-icon size="32px">{{ icon }}</v-icon>
+          </v-btn>
+        </v-card-text>
+      </v-flex>
+    </v-flex>
+    <v-flex cols="6">
+      <v-card-text class="copyright white--text" align="end">
+        &copy; Juro Online Education {{ new Date().getFullYear() }}
+      </v-card-text>
+    </v-flex>
+  </v-footer>
 </template>
 
 <script>
 export default {
-  components: {},
+  data: () => ({
+    links: ["Home", "About Us", "Terms and Condition", "Privacy Policy", "FAQ"],
+    icons: ["mdi-facebook", "mdi-twitter", "mdi-instagram"]
+  }),
   methods: {}
 };
 </script>
 
 <style>
-#footer {
-  background: #2c6195;
+.content {
+  margin: 10px;
 }
 
-#content {
-  margin: 50px;
+.app_download {
+  margin-left: auto;
+  width: 200px;
+}
+
+.copyright {
+  margin-top: 140px;
+}
+
+@media screen and (max-width: 600px) {
+  .app_download {
+    margin-top: 10px;
+    width: 100%;
+  }
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,69 @@
+<template>
+  <v-app-bar absolute prominent color="#3B84CD">
+    <v-flex>
+      <v-img
+        alt="Vuetify Logo"
+        class="shrink ma-6"
+        contain
+        src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+        transition="scale-transition"
+        width="40"
+      />
+    </v-flex>
+    <v-flex>
+      <v-row justify="end" class="ma-6">
+        <v-text-field
+          class="search_text"
+          v-model="search"
+          label="Search Here"
+          single-line
+          clearable
+          solo
+          flat
+        ></v-text-field>
+        <v-btn
+          class="search_button"
+          depressed
+          large
+          height="48px"
+          color="yellow"
+        >
+          <h2><v-icon>mdi-magnify</v-icon></h2>
+        </v-btn>
+      </v-row>
+    </v-flex>
+    <v-col cols="4" align="end">
+      <v-row justify="end">
+        <v-btn text color="white" large>
+          <v-icon class="ma-2">mdi-apps</v-icon> App
+        </v-btn>
+        <v-btn text color="white" large>
+          <v-icon class="ma-2">mdi-account-group</v-icon> Partner with Us
+        </v-btn>
+        <v-btn text color="white" large>
+          <v-icon class="ma-2">mdi-account-tie-voice</v-icon> Customer Care
+        </v-btn>
+      </v-row>
+      <v-row justify="end"></v-row>
+      <v-btn class="mr-3 ml-3 mt-1 white--text" color="#326FAB" large>
+        <v-icon class="ma-2">mdi-account-circle</v-icon> Sign-In
+      </v-btn>
+    </v-col>
+  </v-app-bar>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.search_text {
+  border-top-right-radius: 0;
+  border-bottom-right-radius: 0;
+}
+
+.search_button {
+  border-top-left-radius: 0;
+  border-bottom-left-radius: 0;
+}
+</style>
